17031496060c20b4306b41728735fba351563f84
JaySant <jaysant15@gmail.com>
2022-10-18 23:57:27 -0300

    Requisito - 6, novamente pois deu erro

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
-SELECT MIN(valor) AS faturamento_minimo,
-MAX(valor) AS faturamento_maximo,
-ROUND(AVG(valor),2) AS faturamento_medio,
-SUM(valor) AS faturamento_total
+SELECT MIN(plano.valor) AS faturamento_minimo,
+MAX(plano.valor) AS faturamento_maximo,
+ROUND(AVG(plano.valor),2) AS faturamento_medio,
+ROUND(SUM(plano.valor),2) AS faturamento_total
 FROM SpotifyClone.plano AS plano
 INNER JOIN SpotifyClone.usuario AS u ON u.plano_ID = plano.plano_ID
